8b11d53388e55bad237c631acb183d64561acc99
yohanangulo <76410895+yohanangulo@users.noreply.github.com>
2023-08-19 11:59:00 -0400

    update

--- lib/widgets/expenses_list/expenses_list.dart
+++ lib/widgets/expenses_list/expenses_list.dart
@@ -20,6 +20,12 @@ class ExpensesList extends StatelessWidget {
         onDismissed: (direction) {
           onRemoveExpense(expenses[index]);
         },
+        background: Container(
+          color: Theme.of(context).colorScheme.error.withOpacity(0.75),
+          margin: EdgeInsets.symmetric(
+            horizontal: Theme.of(context).cardTheme.margin!.horizontal,
+          ),
+        ),
         key: ValueKey(expenses[index]),
         child: ExpenseItem(
           expenses[index],
